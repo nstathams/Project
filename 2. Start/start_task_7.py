e0f2383ad781748c93a0c91f02ff23b7e498993b
Nikita <nikitakinztel45@gmail.com>
2025-09-27 20:47:55 +0700

    Update start_task_7.py

--- 2. Start/start_task_7.py
+++ 2. Start/start_task_7.py
@@ -1,1 +1,14 @@
-task9 = 0
+def _iban_lengths(file_path):
+    iban_lengths = {}
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 2:
+                country_code = parts[0]
+                length = int(parts[1])
+                iban_lengths[country_code] = length
+    return iban_lengths
+file_path = 'iban_lenght.txt'
+iban_dict = _iban_lengths(file_path)
+print(iban_dict)
