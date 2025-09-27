20ff5f5ddf089218ca5d61e965e47d875003d78d
Nikita <nikitakinztel45@gmail.com>
2025-09-27 20:53:18 +0700

    Update start_task_11.py

--- 2. Start/start_task_11.py
+++ 2. Start/start_task_11.py
@@ -1,1 +1,12 @@
-task12 = 0
+n = int(input("Введите число n: "))
+def factorial(n):
+    if n < 0:
+        return None
+    elif n == 0:
+        return "1"
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return str(result)
+print(factorial(n))
