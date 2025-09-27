32492d8ccfd8ba30ef87efbac083eb31fe0a2e9e
Nikita <nikitakinztel45@gmail.com>
2025-09-27 20:41:25 +0700

    Update start_task_6.py

--- 2. Start/start_task_6.py
+++ 2. Start/start_task_6.py
@@ -1,1 +1,17 @@
-task7 = 0
+def sorted_numbers(k1, k2):
+    str_k1 = str(k1)
+    str_k2 = str(k2)
+    sorted1 = ''.join(sorted(str_k1))
+    sorted2 = ''.join(sorted(str_k2))
+    return sorted1 == sorted2
+k1 = int(input("Введите число k1: "))
+k2 = k1 + 1
+n = 1
+result = False
+while not result:
+    ck1 = k1 * n
+    ck2 = k2 * n
+    result = sorted_numbers(ck1, ck2)
+    if not result:
+        n += 1
+print(f"Наименьшее n: {n}")
