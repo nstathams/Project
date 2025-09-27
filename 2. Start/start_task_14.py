3b0d2697d235c4bf49adc5736b609d0169c39168
Nikita <nikitakinztel45@gmail.com>
2025-09-27 21:17:00 +0700

    Update start_task_14.py

--- 2. Start/start_task_14.py
+++ 2. Start/start_task_14.py
@@ -1,1 +1,11 @@
-task15 = 0
+def max_chislo(test):
+    max_sum = 0
+    c_sum = 0
+    for num in test:
+        c_sum += num
+        if c_sum < 0:
+            c_sum = 0
+        max_sum = max(max_sum, c_sum)
+    return max_sum
+print(max_chislo([-2, 1, -3, 4, -1, 2, 1, -5, 4])) 
+print(max_chislo([-1, -2, -3]))  
