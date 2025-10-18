ffa279632aff3f5e8bc11d34e4643a7473412051
Nikita <nikitakinztel45@gmail.com>
2025-10-18 16:17:01 +0700

    Update task_4.py

--- 3. Algo/task_4.py
+++ 3. Algo/task_4.py
@@ -1,2 +1,12 @@
-number = int(input("Введите число: "))
-print('Thats the number you entered', number)
+def linear_search(arr, target):
+    for index in range(len(arr)):
+        if arr[index] == target:
+            return index
+    return -1
+
+assert linear_search([5, 3, 7, 1, 4], 1) == 3
+assert linear_search([10, 20, 30, 40], 30) == 2
+assert linear_search([1, 2, 3, 4], 5) == -1
+assert linear_search([1], 1) == 0
+assert linear_search([], 1) == -1
+print("OK!")
