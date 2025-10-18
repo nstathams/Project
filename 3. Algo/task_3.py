095fb1eca1034c9c6fb4d1048fddd1d05fb05b22
Nikita <nikitakinztel45@gmail.com>
2025-10-18 14:19:23 +0700

    Add files via upload

---
+++ 3. Algo/task_3.py
@@ -0,0 +1,3 @@
+y = int(input("Введите число от 0 до 9: "))
+for k in range(0,10):
+    print(f'{k}*{y} = {k*y}')
---
+++ 3. Algo/task_6.py
@@ -0,0 +1,8 @@
+m = int(input("Введите число m: "))
+n = int(input("Введите число n: "))
+if m > n:
+    print("Number m > n")
+elif m < n:
+    print("Number m < n")
+else:
+    print("The numbers are equal")
