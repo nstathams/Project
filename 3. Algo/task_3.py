336add35551c73950f7e4e1dd13e659767dd09ee
Nikita <nikitakinztel45@gmail.com>
2025-10-18 16:11:35 +0700

    Update task_3.py

--- 3. Algo/task_3.py
+++ 3. Algo/task_3.py
@@ -1,3 +1,13 @@
-y = int(input("Введите число от 0 до 9: "))
-for k in range(0,10):
-    print(f'{k}*{y} = {k*y}')
+import random
+
+def quick_sort_deck(deck):
+    if len(deck) < 4:
+        return sorted(deck)
+    pivot = random.choice(deck)
+    left = [card for card in deck if card < pivot]
+    right = [card for card in deck if card > pivot]
+    return quick_sort_deck(left) + [pivot] + quick_sort_deck(right)
+
+deck = [6, 2, 1, 5, 4, 3]
+sorted_deck = quick_sort_deck(deck)
+print(sorted_deck)
